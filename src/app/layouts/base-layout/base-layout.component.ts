2f1d290ece37284a82bb96ff097d08bbe6df7458
David Rachwalik <david.rachwalik@outlook.com>
2022-07-16 12:39:33 -0500

    added base layout

---
+++ src/app/layouts/base-layout/base-layout.component.ts
@@ -0,0 +1,19 @@
+/*
+ * Title: base-layout.component.ts
+ * Author: David Rachwalik
+ * Date: 2022/07/15
+ * Description: Layout (base) component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.scss'],
+})
+export class BaseLayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
