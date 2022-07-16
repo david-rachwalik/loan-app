7bd4c69d16a2e7dd2d74f299a43a61fc814ab03b
David Rachwalik <david.rachwalik@outlook.com>
2022-07-16 12:39:46 -0500

    added a 404 not found page

---
+++ src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,19 @@
+/*
+ * Title: not-found.component.ts
+ * Author: David Rachwalik
+ * Date: 2022/07/15
+ * Description: 404 not found component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
+})
+export class NotFoundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
